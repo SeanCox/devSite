a5d2e5567a3927f9b64b77a75fc68ffaccccf518
SeanCox <sean.cox@mac.com>
2016-11-18 00:29:50 -0800

    [fix] added proper process for calling port in node app

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use('/', express.static('public'))
 
-const port = process.env.port || 1337
+const port = process.env.PORT || 1337
 app.listen(port, ()=>{
   console.log(`devSite is listening on ${port}`)
 })
